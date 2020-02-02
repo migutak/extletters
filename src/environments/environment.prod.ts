f0fc31d62494c3ed642dca82ec1ce695c0c1ddad
Kevin Miguta <migutak@gmail.com>
2020-02-02 19:41:42 +0300

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api: 'http://172.16.204.71:8000',
-  demanddownload: 'http://172.16.204.71:3100',
+  api: 'https://ecollectweb.co-opbank.co.ke',
+  demanddownload: 'https://ecollectweb.co-opbank.co.ke/filesapi',
   filepath: '/home/ecollectadmin/demandletters/'
 };
